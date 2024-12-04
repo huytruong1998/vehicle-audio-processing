8ce568dd6bfcb847de80fb9882784802fea216fb
huytruong1998 <skyzerzlol@gmail.com>
2024-12-04 21:20:45 +0200

    update feature extract get image from mel spectrogram

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Intro to Audio and Speech processing Project
 audio process bus and tram
+
+# https://medium.com/@hasithsura/audio-classification-d37a82d6715
+# https://towardsdatascience.com/cnns-for-audio-classification-6244954665ab
+# https://bamblebam.medium.com/audio-classification-and-regression-using-pytorch-48db77b3a5ec
+CNN is a type of deep learning neural network that classify image really well.
+Can it be use for sound? Yes, We just need to make sound look like an image and shape them like an image and feed it to CNN 
--- feature_extract.py
+++ feature_extract.py
@@ -9,38 +9,88 @@ import matplotlib.pyplot as plt
 
 def getFilePaths(path):
     return [path +'/'+ file for file in os.listdir(path)]
-    
+
 
 
 class SoundDataSet(Dataset):
-    def __init__(self, file_paths, labels, sr,n_fft, duration=5):
-         self.file_paths = file_paths
-         self.labels = labels
-         self.sampled_sr = sr
-         self.n_fft = n_fft
+    def __init__(self, file_paths, labels,categories, sr,n_fft,hop_length, n_mfcc, device, duration=5):
+        self.file_paths = file_paths
+        self.labels = labels
+        
+
+        self.sampled_sr = sr
+        self.n_fft = n_fft
+        self.duration = duration
+        self.hop_length = hop_length
+        self.n_mfcc= n_mfcc
+        self.device = device
+
+        self.c2i={}
+        self.i2c={}
+        self.categories = categories
+        self.images= []
+        for i, category in enumerate(self.categories):
+            self.c2i[category]=i
+            self.i2c[i]=category
+        for index in range(len(file_paths)):
+            log_ms = self.__get_melspectrogram_db__(index)
+            image = self.__melspec_to_image__(log_ms)
+            self.images.append(image)
+
+
+         
     def __len__(self):
         return len(self.file_paths)
-    def __getitem__(self, index):
-        audio_sample_path = self.file_paths[index]
-        label = self.labels[index]
-        signal, sr = librosa.load(audio_sample_path, sr=None)
-        signal_resampled, sr_resampled = self._resample_audio_(signal, sr)
-        return signal_resampled, sr_resampled, label
-        # sd.play(signal_resampled, sr_resampled)
-        # sd.wait()
 
-    def __normalized_mfcc_item__(self, index):
-        signal, sr, label = self.__getitem__(index)
-        win_size= self.n_fft
-        hop_size = win_size//2
-        mfccs_librosa = librosa.feature.mfcc(y=signal, sr=sr, n_fft=self.n_fft, hop_length=hop_size)
-        mfcc_mean = np.mean(mfccs_librosa)
-        return mfccs_librosa, mfcc_mean
+    def __get_item__(self, index):
+        return self.images[index], self.labels[index]
+    
+    def __load_audio__(self, index):
+        audio_sample_path = self.file_paths[index]
+        signal, sr = librosa.load(audio_sample_path, sr=None)
+        # Get the first 5s (duration) of the audio
+        signal = self.__get_audio_duration__(signal, sr)
+        signal, sr = self.__resample_audio__(signal, sr)
+        return signal, sr
+
+    def __get_audio_duration__(self, signal,sr):
+        if signal.shape[0]<self.duration*sr:
+            signal=np.pad(signal,int(np.ceil((self.duration*sr-signal.shape[0])/2)),mode='reflect')
+        else:
+            signal=signal[:self.duration*sr]
+        return signal
+
+    def __resample_audio__(self, signal, sr):
+        if sr != self.sampled_sr:
+            signal_resampled = librosa.resample(signal, orig_sr=sr, target_sr=self.sampled_sr)
+            return signal_resampled, self.sampled_sr
+        return signal, sr
+
+    def __get_melspectrogram_db__(self, index):
+        signal, sr = self.__load_audio__(index)
+        ms = librosa.feature.melspectrogram(y=signal, sr=sr)
+        log_ms = librosa.power_to_db(ms, ref=np.max)
+        return log_ms
+    
+    def __melspec_to_image__(self,log_ms):
+        melspec_norm = self.__melspec_normalization__(log_ms)
+        melspec_min, melspec_max = melspec_norm.min(), melspec_norm.max()
+        melspec_scaled = 255 * (melspec_norm - melspec_min) / (melspec_max - melspec_min)
+        melspec_scaled = melspec_scaled.astype(np.uint8)
+        return melspec_scaled
+    
+    def __melspec_normalization__(self,log_ms):
+        mean = log_ms.mean()
+        std = log_ms.std()
+        melspec_norm = (log_ms - mean) / (std)
+        return melspec_norm
 
     def __plot_mfcc__(self, index):
-        mfccs, _ = self.__normalized_mfcc_item__(index)
+        signal, sr = self.__load_audio__(index)
+        hop_size = self.hop_length//2
+        mfccs_librosa = librosa.feature.mfcc(y=signal, sr=sr, n_fft=self.n_fft, hop_length=hop_size)
         plt.figure()
-        plt.imshow(mfccs, aspect='auto', origin='lower', cmap='viridis')
+        plt.imshow(mfccs_librosa, aspect='auto', origin='lower', cmap='viridis')
         plt.colorbar(format='%+2.0f')
         plt.title("MFCCs")
         plt.xlabel("Time Frames")
@@ -50,7 +100,7 @@ class SoundDataSet(Dataset):
         
 
     def __plot_spectrogram__(self, index):
-        signal, sr, label = self.__getitem__(index)
+        signal, sr = self.__load_audio__(index)
         nfft = self.n_fft
         win_size = nfft
         hop_size = nfft//2
@@ -66,27 +116,23 @@ class SoundDataSet(Dataset):
         plt.tight_layout()
         plt.show()
 
-    def _get_audio_label(self,index):
-        return self.labels[index]
-
-    def _resample_audio_(self, signal, sr):
-        if sr != self.sampled_sr:
-            signal_resampled = librosa.resample(signal, orig_sr=sr, target_sr=self.sampled_sr)
-            return signal_resampled, self.sampled_sr
-        return signal, sr
+    
 
 
 if __name__ == "__main__":
-    audio_bus = getFilePaths('dataset/bus')
-    bus_labels = np.zeros(len(audio_bus))
-
-    audio_tram = getFilePaths('dataset/tram')
-    tram_labels = np.ones(len(audio_tram))
-
-    all_audio = audio_bus + audio_tram
-    all_labels = np.concatenate((bus_labels, tram_labels))
+    audio_paths = []
+    audio_labels = []
+    categories = ['bus', 'tram']
+    for i, category in enumerate(categories):
+        paths = getFilePaths('dataset/' + category)
+        audio_paths += paths
+        audio_labels += [i]*len(paths)
+   
 
     SAMPLE_RATE = 44100
+    n_fft=2048
+    hop_length = 512
+    n_mfcc = 13
 
     if torch.cuda.is_available():
         device = "cuda"
@@ -95,5 +141,6 @@ if __name__ == "__main__":
     print(f"Using device {device}")
 
 
-    audio_data = SoundDataSet(file_paths=all_audio, labels=all_labels, sr=SAMPLE_RATE,n_fft=512)
-    audio_data.__plot_spectrogram__(10)
+    audio_data = SoundDataSet(file_paths=audio_paths, labels=audio_labels,categories=categories, sr=SAMPLE_RATE,n_fft=n_fft, hop_length=hop_length, n_mfcc=n_mfcc, device=device)
+    audio_data.__plot_spectrogram__(40)
+    audio_data.__plot_mfcc__(40)
